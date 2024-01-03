bd06506a91f0ae746715060934712093815abe90
Mohammad <mohammadazadbar1373@gmail.com>
2024-01-03 15:32:49 +0330

    [feature] change Color and Theme

--- app/src/main/java/ir/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/ir/movieapp/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -17,15 +18,15 @@ import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = primaryDark,
+    secondary = primaryDark,
+    background = primaryDark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Purple500,
+    secondary = Teal200,
+    background = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
