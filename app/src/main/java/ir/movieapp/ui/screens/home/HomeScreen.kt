f88de95f7f3999e4f903d465a317e0ab20834c0e
mohammad <mohammadazadbar@yahoo.com>
2024-01-04 16:36:56 +0330

    [feature] add genre section in viewmodel and home

--- app/src/main/java/ir/movieapp/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/ir/movieapp/ui/screens/home/HomeScreen.kt
@@ -1,35 +1,54 @@
 package ir.movieapp.ui.screens.home
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import ir.movieapp.R
 import ir.movieapp.ui.theme.primaryDark
 import ir.movieapp.ui.theme.primaryPink
+import org.intellij.lang.annotations.JdkConstants
+import timber.log.Timber
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun HomeScreen(
-
+    viewModel: HomeViewModel = hiltViewModel()
 ) {
 
     Scaffold(
@@ -59,12 +78,94 @@ fun HomeScreen(
             }
         }
     ) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
         ) {
 
+            item {
+                FilmCategory(
+                    listOf("Movies", "Tv Shows"),
+                    modifier = Modifier,
+                    viewModel = viewModel
+                )
+
+
+            }
+
+            item {
+                Text(
+                    text = "Genres",
+                    modifier = Modifier.padding(8.dp),
+                    fontSize = 22.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(5.dp))
+            }
+
+            item {
+                Genres(
+                    viewModel = viewModel,
+                )
+            }
         }
     }
 
+}
+
+@Composable
+fun FilmCategory(items: List<String>, modifier: Modifier, viewModel: HomeViewModel) {
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        items.forEach {item->
+            Text(
+                text = item,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable {
+
+                    },
+            )
+        }
+    }
+}
+
+@Composable
+fun Genres(
+    viewModel: HomeViewModel
+) {
+    val genres = viewModel.movieGenre.value
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        LazyRow {
+            items(genres.size) { index ->
+                Text(
+                    text = genres[index].name,
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier
+                        .clip(
+                            RoundedCornerShape(
+                                size = 8.dp
+                            )
+                        )
+                        .clickable {
+                            Timber
+                                .tag("TAG")
+                                .e("Genres: %s", genres[index].name)
+                        }
+                )
+            }
+        }
+    }
 }
