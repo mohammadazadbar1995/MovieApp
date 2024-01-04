9d4970be9f275de39acc915a8758cd08775e11b3
mohammad <mohammadazadbar@yahoo.com>
2024-01-04 16:37:11 +0330

    [feature] add api key

--- app/src/main/java/ir/movieapp/util/preview/Constants.kt
+++ app/src/main/java/ir/movieapp/util/preview/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
 
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val SPLASH_SCREEN_DURATION = 3000L
+    const val API_KEY = "d641bdbb66627827cfe96ab940acd175"
 }
