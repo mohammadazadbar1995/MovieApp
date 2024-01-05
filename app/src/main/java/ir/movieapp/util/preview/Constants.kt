38a953dd5d63ed1a12fc6e29efbdcefb98546015
mohammad <mohammadazadbar@yahoo.com>
2024-01-05 21:07:30 +0330

    [feature] add code paging

--- app/src/main/java/ir/movieapp/util/preview/Constants.kt
+++ app/src/main/java/ir/movieapp/util/preview/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val SPLASH_SCREEN_DURATION = 3000L
     const val API_KEY = "d641bdbb66627827cfe96ab940acd175"
+
+    const val STARTING_PAGE_INDEX = 1
+
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
 }
