e170051e12a6ef967dc5c9424e94572b936f4d41
Mohammad <mohammadazadbar1373@gmail.com>
2024-01-06 08:43:10 +0330

    [feature] add repository and function viewmodel

--- app/src/main/java/ir/movieapp/data/repository/MoviesRepository.kt
+++ app/src/main/java/ir/movieapp/data/repository/MoviesRepository.kt
@@ -4,23 +4,33 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import ir.movieapp.data.TMDBApi
+import ir.movieapp.data.paging.PopularMoviesSource
 import ir.movieapp.data.paging.TrendingMoviesSource
+import ir.movieapp.data.remote.response.PopularResponse
 import ir.movieapp.data.remote.response.TrendingResponse
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class MoviesRepository @Inject constructor(private val api: TMDBApi){
+class MoviesRepository @Inject constructor(private val api: TMDBApi) {
 
 
-   fun getTrendingMovies() : Flow<PagingData<TrendingResponse.Movie>> {
-       return Pager(
-           config = PagingConfig(enablePlaceholders = false, pageSize = 27),
-           pagingSourceFactory = {
-               TrendingMoviesSource(api)
-           }
-       ).flow
-   }
+    fun getTrendingMovies(): Flow<PagingData<TrendingResponse.Movie>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = 27),
+            pagingSourceFactory = {
+                TrendingMoviesSource(api)
+            }
+        ).flow
+    }
 
+    fun getPopularMovies(): Flow<PagingData<PopularResponse.Popular>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = 27),
+            pagingSourceFactory = {
+                PopularMoviesSource(api)
+            }
+        ).flow
+    }
 
 
 }
