a7b7a70a55cc9e79f44e8c3f267082e53b5392b6
Mohammad <mohammadazadbar1373@gmail.com>
2024-01-09 12:55:14 +0330

    [feature] improve detail page

--- app/src/main/java/ir/movieapp/ui/screens/detail/MovieBanner.kt
+++ app/src/main/java/ir/movieapp/ui/screens/detail/MovieBanner.kt
@@ -5,20 +5,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.safeGesturesPadding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,12 +21,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.google.accompanist.insets.LocalWindowInsets
@@ -40,7 +37,7 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import ir.movieapp.data.remote.response.MovieDetailResponse
 import ir.movieapp.ui.screens.commons.CircularBackButtons
 import ir.movieapp.ui.screens.commons.CircularFavoriteButtons
-import ir.movieapp.ui.screens.commons.TopAppBar
+import ir.movieapp.ui.screens.commons.VoteAverageRatingIndicator
 import ir.movieapp.ui.theme.AppBarCollapsedHeight
 import ir.movieapp.ui.theme.AppBarExpendedHeight
 import ir.movieapp.ui.theme.primaryDark
@@ -131,7 +128,36 @@ fun MovieBanner(
                         isLink = it
                     }
                 )
+
+
             }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomStart)
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Bottom,
+
+                ) {
+                Text(
+                    text = movieData?.title ?: "",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    )
+
+                VoteAverageRatingIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth(0.17f),
+                    percentage = movieData?.voteAverage?.toFloat() ?: 0f
+                )
+
+            }
+
         }
+
+
     }
 }
