fe319b5149efec669455db6b0bc082a0095e5898
Mohammad <mohammadazadbar1373@gmail.com>
2024-01-10 15:41:59 +0330

    [feature] add Cast Item And Finish Detail

---
+++ app/src/main/java/ir/movieapp/ui/screens/commons/CastItemView.kt
@@ -0,0 +1,63 @@
+package ir.movieapp.ui.screens.commons
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import ir.movieapp.data.remote.response.CreditsResponse
+import ir.movieapp.util.preview.Constants
+
+@Composable
+fun CastItemView(cast: CreditsResponse.Cast) {
+    Column(
+        modifier = Modifier
+            .wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("${Constants.IMAGE_BASE_URL}/${cast.profilePath}")
+                    .crossfade(true)
+                    .build(),
+            ),
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,            // crop the image if it's not a square
+            modifier = Modifier
+                .size(96.dp)
+                .padding(4.dp)
+                .clip(CircleShape)                       // clip to the circle shape
+                .border(2.dp, Color.Gray, CircleShape)   // add a border (optional)
+        )
+
+        Spacer(modifier = Modifier.size(4.dp))
+
+        Text(
+            text = cast.name,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
--- app/src/main/java/ir/movieapp/ui/screens/detail/FilmInfoDetail.kt
+++ app/src/main/java/ir/movieapp/ui/screens/detail/FilmInfoDetail.kt
@@ -1,13 +1,26 @@
 package ir.movieapp.ui.screens.detail
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,11 +28,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.font.FontWeight
@@ -27,15 +45,21 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import ir.movieapp.R
+import ir.movieapp.data.remote.response.CreditsResponse
 import ir.movieapp.data.remote.response.MovieDetailResponse
+import ir.movieapp.ui.screens.commons.CastItemView
 import ir.movieapp.ui.theme.primaryPink
+import ir.movieapp.util.preview.Constants
 
 @Composable
 fun FilmInfoDetail(
     navigator: DestinationsNavigator,
     movieData: MovieDetailResponse,
+    casts: List<CreditsResponse.Cast>?,
 ) {
     Column(
         modifier = Modifier
@@ -66,8 +90,87 @@ fun FilmInfoDetail(
 
         ExpandableText(text = movieData.overview, modifier = Modifier.fillMaxWidth())
 
+        Spacer(modifier = Modifier.height(10.dp))
+
+        if (!casts.isNullOrEmpty()) {
+            CastView(
+                casts = casts,
+            )
+        }
+
+
     }
 }
+
+@Composable
+fun CastView(
+    casts: List<CreditsResponse.Cast>
+) {
+
+
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(top = 4.dp)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(id = R.string.cast),
+                fontSize = 16.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(top = 4.dp)
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.view_all),
+                    fontSize = 16.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                )
+
+                Spacer(modifier = Modifier.width(4.dp))
+
+                IconButton(onClick = {
+
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_chevron_right),
+                        contentDescription = null
+                    )
+                }
+
+            }
+        }
+
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            items(casts.size) {
+                CastItemView(
+                    cast = casts[it]
+                )
+            }
+        }
+
+    }
+}
+
 
 @Composable
 fun ExpandableText(
